a51d47a1836feb822d090ba0e06e96581d961a05
student <student@BTU.local>
2025-12-17 17:41:47 +0400

    image card with text and image and gradient background to the text

--- app/src/main/java/com/example/composepracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/composepracticeapp/MainActivity.kt
@@ -3,71 +3,92 @@ package com.example.composepracticeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            val painter = painterResource(id = R.drawable.image)
+            val description = "image from unsplash"
+            val title = "This is the image i wanted to show you!"
+            Box(
                 modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-
-//                    .requiredWidth(600.dp)
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
             ) {
-                Text("Hello",
-//                    Modifier.clickable {
-//
-//                }
-//                .offset(20.dp,20.dp)
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = title
                 )
-                Spacer(modifier = Modifier.height(50.dp))
+            }
+
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
                 Text(
-                    text = "world",
-//                    modifier = Modifier
-//                        .border(5.dp, Color.Yellow)
-//                        .padding(5.dp)
-//                        .offset(20.dp,20.dp)
-//                        .border(10.dp,Color.Black)
-//                        .padding(10.dp)
+                    title,
+                    style = TextStyle(Color.White, fontSize = 16.sp)
                 )
             }
         }
