2e38539d3e2f0c7fb479810be65f9b811412d42b
student <student@BTU.local>
2025-12-17 16:48:32 +0400

    modifiers

--- app/src/main/java/com/example/composepracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/composepracticeapp/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
@@ -31,43 +39,36 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val constraints = ConstraintSet {
-                val greenBox = createRefFor("greenBox")
-                val redBox = createRefFor("redBox")
-                val blueBox = createRefFor("blueBox")
-
-                val guideline = createGuidelineFromTop(0.5f)
-
-                constrain(greenBox){
-                    top.linkTo(guideline)
-                    start.linkTo(parent.start)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                constrain(redBox){
-                    top.linkTo(parent.top)
-                    start.linkTo(greenBox.end)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                constrain(blueBox){
-                    top.linkTo(parent.top)
-                    start.linkTo(redBox.end)
-                    width = Dimension.value(100.dp)
-                    height = Dimension.value(100.dp)
-                }
-                createHorizontalChain(greenBox,redBox,blueBox)
-            }
-            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
-                Box(modifier = Modifier
+            Column(
+                modifier = Modifier
                     .background(Color.Green)
-                    .layoutId("greenBox"))
-                Box(modifier = Modifier
-                    .background(Color.Red)
-                    .layoutId("redBox"))
-                Box(modifier = Modifier
-                    .background(Color.Blue)
-                    .layoutId("blueBox"))
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+
+//                    .requiredWidth(600.dp)
+            ) {
+                Text("Hello",
+//                    Modifier.clickable {
+//
+//                }
+//                .offset(20.dp,20.dp)
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(
+                    text = "world",
+//                    modifier = Modifier
+//                        .border(5.dp, Color.Yellow)
+//                        .padding(5.dp)
+//                        .offset(20.dp,20.dp)
+//                        .border(10.dp,Color.Black)
+//                        .padding(10.dp)
+                )
             }
         }
     }
