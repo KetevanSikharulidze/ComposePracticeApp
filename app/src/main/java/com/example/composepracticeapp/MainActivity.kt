fe2e88feb27954bf25a083c6b834bbdefdb6d605
student <student@BTU.local>
2025-12-17 18:11:54 +0400

    outdated design things... idk

--- app/src/main/java/com/example/composepracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/composepracticeapp/MainActivity.kt
@@ -3,88 +3,69 @@ package com.example.composepracticeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val fontFamily = FontFamily(
-            Font(R.font.lexend_thin, FontWeight.Thin),
-            Font(R.font.lexend_black, FontWeight.Black),
-            Font(R.font.lexend_light, FontWeight.Light),
-            Font(R.font.lexend_extralight, FontWeight.ExtraLight),
-            Font(R.font.lexend_regular, FontWeight.Normal),
-            Font(R.font.lexend_medium, FontWeight.Medium),
-            Font(R.font.lexend_semibold, FontWeight.SemiBold),
-            Font(R.font.lexend_bold, FontWeight.Bold),
-            Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
-        )
-
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF101010))
-            )
-            Text(
-                text = buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(
-                            color = Color.Blue,
-                            fontSize = 50.sp
-                        )){
-                        append("J")
+            val snackbarHostState = remember { SnackbarHostState() }
+            var textFieldState by remember { mutableStateOf("") }
+            val scope = rememberCoroutineScope()
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+            ) { paddingValues ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues) // paddingValues ensures the content doesn't clash with system UI
+                        .padding(horizontal = 30.dp) // creates visual padding inside the screen
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        label = { Text("Enter your name") },
+                        onValueChange = { textFieldState = it },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                message = "Hello $textFieldState"
+                            )
+                        }
+                    }) {
+                        Text("Please greet me")
                     }
-                    append("etpack")
-                    withStyle(
-                        style = SpanStyle(
-                            color = Color.Blue,
-                            fontSize = 50.sp
-                        )){
-                        append("C")
-                    }
-                    append("ompose")
-                },
-                color = Color.White,
-                fontSize = 30.sp,
-                fontFamily = fontFamily,
-                fontWeight = FontWeight.Bold,
-//                fontStyle = FontStyle.Italic,
-                textAlign = TextAlign.Center,
-                textDecoration = TextDecoration.LineThrough
-            )
+                }
+            }
         }
     }
 }
-
